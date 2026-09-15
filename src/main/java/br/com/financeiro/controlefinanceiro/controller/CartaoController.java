b002ac36aa51f6b61be4940fec74d28d633b329b
wilsondebus <wilsonddebus@gmail.com>
2026-09-15 15:06:29 -0300

    removendo cartao.html e ajutando CartaoController

--- src/main/java/br/com/financeiro/controlefinanceiro/controller/CartaoController.java
+++ src/main/java/br/com/financeiro/controlefinanceiro/controller/CartaoController.java
@@ -1,56 +1,16 @@
 package br.com.financeiro.controlefinanceiro.controller;
 
-import br.com.financeiro.controlefinanceiro.model.CartaoCredito;
-import br.com.financeiro.controlefinanceiro.service.CartaoService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/cartoes")
 public class CartaoController {
 
-    private final CartaoService service;
-
-    public CartaoController(
-            CartaoService service
-    ) {
-        this.service = service;
-    }
-
     @GetMapping
-    public String listar(Model model) {
+    public String redirecionarParaPagamentos() {
 
-        model.addAttribute(
-                "cartoes",
-                service.listar()
-        );
-
-        model.addAttribute(
-                "novoCartao",
-                new CartaoCredito()
-        );
-
-        return "cartoes";
-    }
-
-    @PostMapping("/salvar")
-    public String salvar(
-            @ModelAttribute CartaoCredito cartao
-    ) {
-
-        service.salvar(cartao);
-
-        return "redirect:/cartoes";
-    }
-
-    @GetMapping("/excluir/{id}")
-    public String excluir(
-            @PathVariable Long id
-    ) {
-
-        service.excluir(id);
-
-        return "redirect:/cartoes";
+        return "redirect:/pagamentos";
     }
 }
